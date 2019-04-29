73e4509bd86709207f8845750d11a75f4bf509b1
yas1thverana <yaswanth.tadavarthy@veranahealth.com>
2019-04-29 19:11:06 +0530

    Button Reusable Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { VHButton } from "../src/Components/index";
+import "./App.css";
 
 function App() {
   return (
@@ -10,14 +11,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <VHButton text={"HI"} />
       </header>
     </div>
   );
